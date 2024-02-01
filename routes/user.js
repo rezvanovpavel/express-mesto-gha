2c68f403f498c36c3087b4da322f305ea8f76f75
Pavel Rezvanov <rezvanoffpavel@yandex.ru>
2024-02-02 02:37:11 +0300

    PR14

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.get('/', getUsers);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().length(24).hex().required(),
+    userId: Joi.string().length(24).hex().required(),
   }),
 }), getUser);
 
